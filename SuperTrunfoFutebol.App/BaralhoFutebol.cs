80282a7c94474b841c1c567fd232c61547b39bff
Romuloow <romulo.afo2006@gmail.com>
2026-09-22 13:44:57 -0300

    Correção valores das cartas

--- SuperTrunfoFutebol.App/BaralhoFutebol.cs
+++ SuperTrunfoFutebol.App/BaralhoFutebol.cs
@@ -9,16 +9,16 @@ namespace SuperTrunfoFutebol
         {
             List<Jogador> cartas = new List<Jogador>();
 
-            cartas.Add(new Jogador("Pelé", "Santos", "Atacante", 767, 3, 200, 100));
-            cartas.Add(new Jogador("Ronaldo Fenômeno", "Seleção Brasileira", "Atacante", 352, 2, 100, 97));
-            cartas.Add(new Jogador("Zico", "Flamengo", "Meia", 508, 0, 150, 93));
-            cartas.Add(new Jogador("Romário", "Seleção Brasileira", "Atacante", 700, 1, 90, 95));
-            cartas.Add(new Jogador("Ronaldinho Gaúcho", "Seleção Brasileira", "Meia", 350, 1, 180, 94));
-            cartas.Add(new Jogador("Garrincha", "Botafogo", "Ponta", 232, 2, 85, 92));
-            cartas.Add(new Jogador("Neymar Jr", "Santos", "Atacante", 440, 0, 200, 93));
-            cartas.Add(new Jogador("Cafu", "São Paulo", "Lateral", 12, 2, 60, 88));
-            cartas.Add(new Jogador("Rivaldo", "Palmeiras", "Meia", 380, 1, 120, 90));
-            cartas.Add(new Jogador("Kaká", "Milan", "Meia", 300, 0, 110, 91));
+            cartas.Add(new Jogador("Pelé", "Santos", "Atacante", 80, 3, 40, 100));
+            cartas.Add(new Jogador("Ronaldo Fenômeno", "Seleção Brasileira", "Atacante", 62, 2, 31, 97));
+            cartas.Add(new Jogador("Zico", "Flamengo", "Meia", 66, 0, 20, 93));
+            cartas.Add(new Jogador("Romário", "Seleção Brasileira", "Atacante", 55, 1, 14, 95));
+            cartas.Add(new Jogador("Ronaldinho Gaúcho", "Seleção Brasileira", "Meia", 33, 1, 20, 94));
+            cartas.Add(new Jogador("Garrincha", "Botafogo", "Ponta", 17, 2, 27, 92));
+            cartas.Add(new Jogador("Neymar Jr", "Santos", "Atacante", 77, 0, 59, 93));
+            cartas.Add(new Jogador("Cafu", "São Paulo", "Lateral", 5, 2, 32, 88));
+            cartas.Add(new Jogador("Rivaldo", "Palmeiras", "Meia", 35, 1, 17, 90));
+            cartas.Add(new Jogador("Kaká", "Milan", "Meia", 30, 0, 25, 91));
 
             return cartas;
         }
--- SuperTrunfoFutebol.App/Jogador.cs
+++ SuperTrunfoFutebol.App/Jogador.cs
@@ -1,66 +1,66 @@
-using System;
+    using System;
 
-namespace SuperTrunfoFutebol
-{
-
-    public class Jogador
+    namespace SuperTrunfoFutebol
     {
-        public string Nome { get; set; }
-        public string Time { get; set; }
-        public string Posicao { get; set; }
-        public int Gols { get; set; }
-        public int Copas { get; set; }
-        public int Assistencias { get; set; }
-        public int NotaGeral { get; set; }
 
-        public Jogador(string nome, string time, string posicao, int gols, int copas, int assistencias, int notaGeral)
+        public class Jogador
         {
-            if (notaGeral < 0 || notaGeral > 100)
+            public string Nome { get; set; }
+            public string Time { get; set; }
+            public string Posicao { get; set; }
+            public int Gols { get; set; }
+            public int Copas { get; set; }
+            public int Assistencias { get; set; }
+            public int NotaGeral { get; set; }
+
+            public Jogador(string nome, string time, string posicao, int gols, int copas, int assistencias, int notaGeral)
             {
-                throw new ArgumentException("A nota geral deve estar entre 0 e 100.");
+                if (notaGeral < 0 || notaGeral > 100)
+                {
+                    throw new ArgumentException("A nota geral deve estar entre 0 e 100.");
+                }
+
+                if (gols < 0 || copas < 0 || assistencias < 0)
+                {
+                    throw new ArgumentException("Os atributos numéricos não podem ser negativos.");
+                }
+
+                Nome = nome;
+                Time = time;
+                Posicao = posicao;
+                Gols = gols;
+                Copas = copas;
+                Assistencias = assistencias;
+                NotaGeral = notaGeral;
             }
 
-            if (gols < 0 || copas < 0 || assistencias < 0)
+            public void ExibirCarta()
             {
-                throw new ArgumentException("Os atributos numéricos não podem ser negativos.");
+                Console.WriteLine($"----- {Nome} -----");
+                Console.WriteLine($"Time: {Time}");
+                Console.WriteLine($"Posição: {Posicao}");
+                Console.WriteLine($"1 - Gols: {Gols}");
+                Console.WriteLine($"2 - Copas do Mundo: {Copas}");
+                Console.WriteLine($"3 - Assistências: {Assistencias}");
+                Console.WriteLine($"4 - Nota Geral: {NotaGeral}");
+                Console.WriteLine("---------------------");
             }
 
-            Nome = nome;
-            Time = time;
-            Posicao = posicao;
-            Gols = gols;
-            Copas = copas;
-            Assistencias = assistencias;
-            NotaGeral = notaGeral;
-        }
-
-        public void ExibirCarta()
-        {
-            Console.WriteLine($"----- {Nome} -----");
-            Console.WriteLine($"Time: {Time}");
-            Console.WriteLine($"Posição: {Posicao}");
-            Console.WriteLine($"1 - Gols: {Gols}");
-            Console.WriteLine($"2 - Copas do Mundo: {Copas}");
-            Console.WriteLine($"3 - Assistências: {Assistencias}");
-            Console.WriteLine($"4 - Nota Geral: {NotaGeral}");
-            Console.WriteLine("---------------------");
-        }
-
-        public int ObterAtributo(int opcao)
-        {
-            switch (opcao)
+            public int ObterAtributo(int opcao)
             {
-                case 1:
-                    return Gols;
-                case 2:
-                    return Copas;
-                case 3:
-                    return Assistencias;
-                case 4:
-                    return NotaGeral;
-                default:
-                    throw new ArgumentException("Opção de atributo inválida. Escolha um número entre 1 e 4.");
+                switch (opcao)
+                {
+                    case 1:
+                        return Gols;
+                    case 2:
+                        return Copas;
+                    case 3:
+                        return Assistencias;
+                    case 4:
+                        return NotaGeral;
+                    default:
+                        throw new ArgumentException("Opção de atributo inválida. Escolha um número entre 1 e 4.");
+                }
             }
         }
     }
-}
